44cb3c87774e0afe7404df724fc4dd7d603b5c77
Kodewarlock <dapious99@gmail.com>
2025-04-09 00:01:59 +0100

    added small content and set up

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>Image div</div>
+      <div className="text-center">
+        <h3 className="font-medium text-red-600">
+          Competence and experience for your health Family doctor Rheindahlen
+        </h3>
+        <p className="font-extralight">
+          Your own health is a very personal matter. We at your Rheindahlen
+          family doctor understand this, and we repay the trust you place in us
+          with maximum commitment. Because the well-being of our patients is our
+          driving force.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
